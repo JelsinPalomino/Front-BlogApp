f56a2d728f346def2817bf470eec8413cde5286c
JelsinPalomino <jstpalomino@hotmail.com>
2023-03-31 00:01:36 -0500

    Register, arreglado desde el back

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
-import axios from "axios";
+import { Link, useNavigate } from 'react-router-dom'
 
 const Register = () => {
   const [inputs, setInputs] = useState({
@@ -8,18 +7,36 @@ const Register = () => {
     email: "",
     password: "",
   })
+  const [err, setError] = useState(null)
+  const navigate = useNavigate()
 
   const handleChange = e => {
     setInputs(prev =>({...prev, [e.target.name]: e.target.value}))
   }
 
-  const handleSubmit = async e => {
+  const handleSubmit = async (e) => {
     e.preventDefault()
-    try {
-      const res = await axios.post("/auth/register", inputs)
-      console.log(res)
+    try {   
+        
+      const res = await fetch("http://localhost:8800/api/auth/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json" 
+        },
+        body: JSON.stringify(inputs)
+      })
+      
+      if (res.status === 409) {
+        const data = await res.text();
+        setError(data);
+      } else {
+        const data = await res.json();
+        navigate("/login")
+        /* console.log(data) */
+      }
+
     } catch(err) {
-      console.log(err)
+      setError(err.response.data)
     }
   }
 
@@ -31,7 +48,7 @@ const Register = () => {
             <input required type="email" placeholder='email' name='email' onChange={handleChange} />
             <input required type="password" placeholder='password' name='password' onChange={handleChange} />
             <button onClick={handleSubmit}>Register</button>
-            <p>This is an error!</p>
+            {err && <p>{err}!</p>}
             <span>Do you have an account? <Link to="/login">Login</Link>
             </span>
         </form>
